7b689df4226d19ab8046d35a2a692bd8d7babaf3
Elder Moraes <elder@eldermoraes.com>
2023-03-20 12:32:49 -0300

    Ajustes nas mensagens

--- compra-saga/src/main/java/com/eldermoraes/SagaRoute.java
+++ compra-saga/src/main/java/com/eldermoraes/SagaRoute.java
@@ -26,30 +26,30 @@ public class SagaRoute extends RouteBuilder {
 
         //Saga
         from("direct:saga").saga().propagation(SagaPropagation.REQUIRES_NEW).log("Iniciando a transação")
-            .to("direct:newPedido").log("Criando novo pedido com id ${header.id}")
-            .to("direct:newPedidoValor").log("Reservando o crédito")
+            .to("direct:newPedido").log("Pedido ${header.id} criado. Saga ${body}.")
+            .to("direct:newPedidoValor").log("Credito do pedido ${header.id} no valor de BRL ${header.valor} reservado para a saga ${body}")
             .to("direct:finaliza").log("Feito!");
 
         //Pedido service
         from("direct:newPedido").saga().propagation(SagaPropagation.MANDATORY)
             .compensation("direct:cancelPedido")
             .transform().header(Exchange.SAGA_LONG_RUNNING_ACTION)
-            .bean(pedidoService,"newPedido").log("Pedido ${body} criado");
+            .bean(pedidoService,"newPedido").log("Criando novo pedido com id ${header.id}");
 
         from("direct:cancelPedido")
             .transform().header(Exchange.SAGA_LONG_RUNNING_ACTION)
-            .bean(pedidoService,"cancelPedido").log("Pedido ${body} cancelado");
+            .bean(pedidoService,"cancelPedido").log("Pedido ${body} compensado");
 
         //Credito service
 
         from("direct:newPedidoValor").saga().propagation(SagaPropagation.MANDATORY)
             .compensation("direct:cancelPedidoValor")
             .transform().header(Exchange.SAGA_LONG_RUNNING_ACTION)
-            .bean(creditoService,"newPedidoValor").log("Credito do pedido ${header.id} no valor de ${header.valor} reservado para a saga ${body}");
+            .bean(creditoService,"newPedidoValor").log("Reservando o crédito");
 
         from("direct:cancelPedidoValor")
             .transform().header(Exchange.SAGA_LONG_RUNNING_ACTION)
-            .bean(creditoService,"cancelPedidoValor").log("Credito do pedido ${header.id} no valor de ${header.valor} cancelado para a saga ${body}");
+            .bean(creditoService,"cancelPedidoValor").log("Credito compensado para a saga ${body}");
         
         
         //Finaliza
